e46faa4ec0e949f13af27aae6015eefa25bcd6c6
Bryan03A <hacker03291997@gmail.com>
2025-07-07 23:03:55 -0500

    feat: :lock: update MongoDB URIs and CORS to use new infrastructure
    - Replaced MongoDB Atlas URIs with new IP-based self-hosted URIs- Set CORS to allow frontend from http://54.166.118.216:9090

--- backup-chat-service/app.py
+++ backup-chat-service/app.py
@@ -11,11 +11,11 @@ import time
 app = Flask(__name__)
 
 # Enable CORS for requests from localhost:8080
-CORS(app, origins=["http://54.173.251.44:9090"])
+CORS(app, origins=["http://54.166.118.216:9090"])
 
 # MongoDB connection URLs (Main database and Backup)
-uri = "mongodb+srv://MicroserviceDev:1997999@cluster0.hdqpd.mongodb.net/ChatServiceDB?retryWrites=true&w=majority"
-uri_backup = "mongodb+srv://MicroserviceDev:1997999@cluster0.hdqpd.mongodb.net/BackupServiceDB?retryWrites=true&w=majority"
+uri = "mongodb://admin:admin123@35.175.23.86:27017/ChatServiceDB"
+uri_backup = "mongodb://admin:admin123@35.175.23.86:27017/BackupServiceDB"
 
 # Connect to the main database
 client = MongoClient(uri)
