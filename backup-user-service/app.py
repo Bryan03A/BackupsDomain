22b92e521b1b1e25823b6f3956a39afbfa37320c
Bryan03A <hacker03291997@gmail.com>
2025-07-08 00:00:57 -0500

    fix: :construction_worker: update database connections and CORS for Docker deployment
    - Added authSource=admin in MongoDB connection strings to fix auth errors- Changed database IP, user, and password for Docker-hosted DBs- Configured CORS to accept calls from interface frontend

--- backup-user-service/app.py
+++ backup-user-service/app.py
@@ -11,13 +11,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 # Habilitar CORS para solicitudes de localhost:8080
-CORS(app, origins=["http://54.173.251.44:9090"])
+CORS(app, origins=["http://54.166.118.216:9090"])
 
 # URL de conexión a PostgreSQL (Supabase)
-POSTGRES_URI = "postgresql://postgres.imfqyzgimtercyyqeqof:1997Guallaba@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+POSTGRES_URI = "postgresql://admin:admin123@23.23.135.253:5432/mydb"
 
 # URL de conexión a MongoDB (Backup)
-MONGO_URI = "mongodb+srv://MicroserviceDev:1997999@cluster0.hdqpd.mongodb.net/BackupServiceDB?retryWrites=true&w=majority"
+MONGO_URI = "mongodb://admin:admin123@35.175.23.86:27017/BackupServiceDB?authSource=admin"
 
 # Conectar a PostgreSQL
 def connect_postgres():
